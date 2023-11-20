d1969d964a017825bb8dac0e9d57a4f9c9410ea6
jahid <jahidjob5@outlook.com>
2023-11-20 11:54:48 +0300

    Slice

---
+++ slice.js
@@ -0,0 +1,13 @@
+// console.log("HELLO TERMINAL");
+
+
+const theNumber = [2, 4, 45, 36, 42553, 324, 52333,];
+const pull = theNumber.slice(2, 4);
+console.log(pull);
+// [45, 36]
+
+const theNumber2 = [34, 34, 12, 43, 24, 2, 5];
+const pull1 = theNumber2.splice(2, 5);
+console.log(pull1);
+
+// console.log(theNumber2);
